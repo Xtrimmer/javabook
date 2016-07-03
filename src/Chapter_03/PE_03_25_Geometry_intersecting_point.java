e7a5354140a7a8f068ed6142a81e183eaa64e661
Jeff Trimmer <jtrimmer@gmail.com>
2016-07-03 09:34:20 -0500

    Added Chapter 8 exercise 31. Updated comments in Chapter 03 exercise 25.

--- src/Chapter_03/PE_03_25_Geometry_intersecting_point.java
+++ src/Chapter_03/PE_03_25_Geometry_intersecting_point.java
@@ -10,6 +10,12 @@ import java.util.Scanner;
  * This linear equation can be solved using Cramer’s rule.
  * If the equation has no solutions, the two lines are parallel.
  * This is a program that prompts the user to enter four points and displays the intersecting point.
+ *
+ *      Enter x1, y1, x2, y2, x3, y3, x4, y4: 2 2 5 -1.0 4.0 2.0 -1.0 -2.0 (enter)
+ *      The intersecting point is at (2.88889, 1.1111)
+ *
+ *      Enter x1, y1, x2, y2, x3, y3, x4, y4: 2 2 7 6.0 4.0 2.0 -1.0 -2.0 (enter)
+ *      The two lines are parallel
  */
 public class PE_03_25_Geometry_intersecting_point {
     public static void main(String[] args) {
