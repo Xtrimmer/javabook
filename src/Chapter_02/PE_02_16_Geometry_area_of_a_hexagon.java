550f8a581407aa80d07ea67475b10573ab7a58ab
Jeff Trimmer <jtrimmer@gmail.com>
2016-07-29 05:45:39 -0500

    Added Chapter 12 exercise 01.

--- src/Chapter_02/PE_02_16_Geometry_area_of_a_hexagon.java
+++ src/Chapter_02/PE_02_16_Geometry_area_of_a_hexagon.java
@@ -6,7 +6,13 @@ import java.util.Scanner;
  *
  *      area = (3 * √3 / 2) * s^2
  *
- * where s is the length of a side.
+ * where s is the length of a side. Here is a sample run:
+ *
+ *      Enter the side: 5.5
+ *      The area of the hexagon is 78.5918
+ *
+ * NOTE: Per the book's erata at http://www.cs.armstrong.edu/liang/intro10e/errata.html,
+ * In the output box for Programming Exercise 2.16, change 78.5895 to 78.5918.
  */
 public class PE_02_16_Geometry_area_of_a_hexagon {
     public static void main(String[] args) {
