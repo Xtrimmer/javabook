10f4ed3c24be5b0f2f86dde35974758682e43760
Jeff Trimmer <jmt41860@ucmo.edu>
2016-10-02 19:25:15 -0500

    Added Chapter 15 Exercise 24.

---
+++ src/Chapter_15/PE_15_24_Animation_palindrome.java
@@ -0,0 +1,8 @@
+package Chapter_15;
+
+/**
+ * (Animation: palindrome) Write a program that animates a palindrome swing as
+ * shown in Figure 15.31. Press/release the mouse to pause/resume the animation.
+ */
+public class PE_15_24_Animation_palindrome {
+}
