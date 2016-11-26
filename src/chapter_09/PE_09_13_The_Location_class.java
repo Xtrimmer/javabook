265208c2d548225a63fe6d2c10a9cd162e912ed5
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-26 14:34:07 -0600

    Modified Chapter 09 and 10 Exercises.

--- src/chapter_09/PE_09_13_The_Location_class.java
+++ src/chapter_09/PE_09_13_The_Location_class.java
@@ -1,7 +1,5 @@
 package chapter_09;
 
-import utility.Location;
-
 import java.util.Scanner;
 
 /**
@@ -54,5 +52,39 @@ public class PE_09_13_The_Location_class {
             }
         }
         return m;
+    }
+
+    private static class Location {
+        public final int row;
+        public final int column;
+        final double maxValue;
+
+        Location(int row, int column, double maxValue) {
+            this.row = row;
+            this.column = column;
+            this.maxValue = maxValue;
+        }
+
+        static Location locateLargest(double[][] a) {
+            Location location = new Location(0, 0, a[0][0]);
+            for (int r = 0; r < a.length; r++) {
+                for (int c = 0; c < a[r].length; c++) {
+                    if (a[r][c] > location.maxValue)
+                        location = new Location(r, c, a[r][c]);
+                }
+            }
+            return location;
+        }
+
+        @Override
+        public String toString() {
+            String formattedMaxValue;
+            if (maxValue % 1.0 != 0)
+                formattedMaxValue = String.format("%s", maxValue);
+            else
+                formattedMaxValue = String.format("%.0f", maxValue);
+            return String.format("The location of the largest element is %s at (%d, %d)",
+                    formattedMaxValue, row, column);
+        }
     }
 }
--- src/chapter_10/PE_10_02_The_BMI_class.java
+++ src/chapter_10/PE_10_02_The_BMI_class.java
@@ -1,7 +1,5 @@
 package chapter_10;
 
-import utility.BMI;
-
 /**
  * (The BMI class) Add the following new constructor in the BMI class:
  *
@@ -14,5 +12,56 @@ public class PE_10_02_The_BMI_class {
         BMI bmi = new BMI("Jeff", 38, 185, 6, 0);
         System.out.printf("%s is age %s and is %s\" tall%nwith a weight of %s lbs.%nHis status is %s.",
                 bmi.getName(), bmi.getAge(), bmi.getHeight(), bmi.getWeight(), bmi.getStatus());
+    }
+
+    private static class BMI {
+        static final double KILOGRAMS_PER_POUND = 0.45359237;
+        static final double METERS_PER_INCH = 0.0254;
+        private final String name;
+        private final int age;
+        private final double weight; // in pounds
+        private final double height; // in inches
+
+        BMI(String name, int age, double weight, double feet,
+            double inches) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.height = (feet * 12) + inches;
+        }
+
+        public String getStatus() {
+            double bmi = getBMI();
+            if (bmi < 18.5)
+                return "Underweight";
+            else if (bmi < 25)
+                return "Normal";
+            else if (bmi < 30)
+                return "Overweight";
+            else
+                return "Obese";
+        }
+
+        double getBMI() {
+            double bmi = weight * KILOGRAMS_PER_POUND /
+                    ((height * METERS_PER_INCH) * (height * METERS_PER_INCH));
+            return Math.round(bmi * 100) / 100.0;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        int getAge() {
+            return age;
+        }
+
+        double getWeight() {
+            return weight;
+        }
+
+        double getHeight() {
+            return height;
+        }
     }
 }
