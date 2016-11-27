bedc6638dd600d38f95d72f083c9c762cef761de
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-26 19:01:36 -0600

    Modified Chapter 09 Exercise 07.

--- src/chapter_09/PE_09_07_The_Account_class.java
+++ src/chapter_09/PE_09_07_The_Account_class.java
@@ -1,6 +1,6 @@
 package chapter_09;
 
-import utility.Account;
+import java.util.Date;
 
 /**
  * (The Account class) Design a class named Account that contains:
@@ -43,6 +43,67 @@ public class PE_09_07_The_Account_class {
         System.out.printf("   Balance:          $%,.2f%n", account.getBalance());
         System.out.printf("   Monthly interest: $%,.2f%n", account.getMonthlyInterest());
         System.out.printf("   Date Created:     %s%n", account.getDateCreated().toString());
+    }
+
+    private static class Account {
+        private static double annualInterestRate = 0;
+        private final Date dateCreated = new Date();
+        private int id = 0;
+        private double balance = 0;
+
+        Account() {
+        }
+
+        Account(int id, double balance) {
+            this.id = id;
+            this.balance = balance;
+        }
+
+        static double getAnnualInterestRate() {
+            return annualInterestRate;
+        }
+
+        static void setAnnualInterestRate(double annualInterestRate) {
+            Account.annualInterestRate = annualInterestRate;
+        }
+
+        int getId() {
+            return id;
+        }
+
+        void setId(int id) {
+            this.id = id;
+        }
+
+        double getBalance() {
+            return balance;
+        }
+
+        void setBalance(double balance) {
+            this.balance = balance;
+        }
+
+        Date getDateCreated() {
+            return dateCreated;
+        }
+
+        double getMonthlyInterest() {
+            return balance * (getMonthlyInterestRate() / 100.0);
+        }
+
+        double getMonthlyInterestRate() {
+            return annualInterestRate / 12;
+        }
+
+        void withdraw(double amount) {
+            if (balance >= amount) {
+                balance -= amount;
+            }
+        }
+
+        void deposit(double amount) {
+            balance += amount;
+        }
     }
 }
 
