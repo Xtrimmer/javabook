6953f19d12e754c5218a111ca93beaebbb6afb42
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-27 08:50:39 -0600

    Updated Chapter 10 Exercise 12.

--- src/chapter_10/PE_10_12_Geometry_the_Triangle2D_class.java
+++ src/chapter_10/PE_10_12_Geometry_the_Triangle2D_class.java
@@ -80,65 +80,65 @@ public class PE_10_12_Geometry_the_Triangle2D_class {
         private MyPoint p2;
         private MyPoint p3;
 
-        public Triangle2D() {
+        Triangle2D() {
             this(new MyPoint(0, 0),
                     new MyPoint(1, 1),
                     new MyPoint(2, 5));
         }
 
-        public Triangle2D(MyPoint p1, MyPoint p2, MyPoint p3) {
+        Triangle2D(MyPoint p1, MyPoint p2, MyPoint p3) {
             this.p1 = p1;
             this.p2 = p2;
             this.p3 = p3;
         }
 
-        public MyPoint getP1() {
+        MyPoint getP1() {
             return p1;
         }
 
-        public void setP1(MyPoint p1) {
+        void setP1(MyPoint p1) {
             this.p1 = p1;
         }
 
-        public MyPoint getP2() {
+        MyPoint getP2() {
             return p2;
         }
 
-        public void setP2(MyPoint p2) {
+        void setP2(MyPoint p2) {
             this.p2 = p2;
         }
 
-        public MyPoint getP3() {
+        MyPoint getP3() {
             return p3;
         }
 
-        public void setP3(MyPoint p3) {
+        void setP3(MyPoint p3) {
             this.p3 = p3;
         }
 
-        public double getArea() {
+        double getArea() {
             return Math.abs(
                     (p1.getX() * (p2.getY() - p3.getY())
                             + p2.getX() * (p3.getY() - p1.getY())
                             + p3.getX() * (p1.getY() - p2.getY())) / 2d);
         }
 
-        public double getPerimeter() {
+        double getPerimeter() {
             return p1.distance(p2) + p2.distance(p3) + p3.distance(p1);
         }
 
-        public boolean contains(MyPoint p) {
+        boolean contains(MyPoint p) {
             double area1 = new Triangle2D(p, p1, p2).getArea();
             double area2 = new Triangle2D(p, p2, p3).getArea();
             double area3 = new Triangle2D(p, p3, p1).getArea();
             return Math.abs(area1 + area2 + area3 - getArea()) < 0.00000000001;
         }
 
-        public boolean contains(Triangle2D t) {
+        boolean contains(Triangle2D t) {
             return contains(t.p1) && contains(t.p2) && contains(t.p3);
         }
 
-        public boolean overlaps(Triangle2D t) {
+        boolean overlaps(Triangle2D t) {
             Line2D[] linesT1 = {
                     new Line2D.Double(p1.getX(), p1.getY(), p2.getX(), p2.getY()),
                     new Line2D.Double(p2.getX(), p2.getY(), p3.getX(), p3.getY()),
@@ -158,7 +158,7 @@ public class PE_10_12_Geometry_the_Triangle2D_class {
         }
     }
 
-    static class MyPoint {
+    private static class MyPoint {
         private final double x;
         private final double y;
 
