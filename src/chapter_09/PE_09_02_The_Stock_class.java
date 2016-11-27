d6466c598888af23d051a6437e5c646ffdf7295d
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-26 18:40:10 -0600

    Modified Chapter 09 Exercise 02.

--- src/chapter_09/PE_09_02_The_Stock_class.java
+++ src/chapter_09/PE_09_02_The_Stock_class.java
@@ -1,10 +1,9 @@
 package chapter_09;
 
-import utility.Stock;
-
 /**
  * (The Stock class) Following the example of the Circle class in Section 9.2,
  * design a class named Stock that contains:
+ *
  * - A string data field named symbol for the stock’s symbol.
  * - A string data field named name for the stock’s name.
  * - A double data field named previousClosingPrice that stores the stock
@@ -14,6 +13,7 @@ import utility.Stock;
  * - A constructor that creates a stock with the specified symbol and name.
  * - A method named getChangePercent() that returns the percentage changed
  *   from previousClosingPrice to currentPrice.
+ *
  * Draw the UML diagram for the class and then implement the class. Write a test
  * program that creates a Stock object with the stock symbol ORCL, the name
  * Oracle Corporation, and the previous closing price of 34.5. Set a new current
@@ -21,10 +21,43 @@ import utility.Stock;
  */
 public class PE_09_02_The_Stock_class {
     public static void main(String[] args) {
-        Stock stock = new Stock("ORCL", "Oracle Corporation", 34.5);
+        Stock stock = new Stock("ORCL", "Oracle Corporation");
+        stock.setPreviousClosingPrice(34.5);
         stock.setCurrentPrice(34.35);
         System.out.println("The price-change percentage of " + stock.getName() +
                 " (" + stock.getSymbol() + ")\n" + " is " + stock.getChangePercent() + "%");
+    }
+
+    private static class Stock {
+        private final String symbol;
+        private final String name;
+        private double previousClosingPrice;
+        private double currentPrice;
+
+        Stock(String symbol, String name) {
+            this.symbol = symbol;
+            this.name = name;
+        }
+
+        public void setPreviousClosingPrice(double previousClosingPrice) {
+            this.previousClosingPrice = previousClosingPrice;
+        }
+
+        String getSymbol() {
+            return symbol;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        void setCurrentPrice(double currentPrice) {
+            this.currentPrice = currentPrice;
+        }
+
+        double getChangePercent() {
+            return (currentPrice - previousClosingPrice) / previousClosingPrice * 100;
+        }
     }
 }
 
