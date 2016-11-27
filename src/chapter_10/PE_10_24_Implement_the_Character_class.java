8666d3505001359865f979f8facc55e6e995f81f
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-27 09:00:46 -0600

    Consolidated Chapter 10 Exercise 24.

--- src/chapter_10/PE_10_24_Implement_the_Character_class.java
+++ src/chapter_10/PE_10_24_Implement_the_Character_class.java
@@ -1,7 +1,5 @@
 package chapter_10;
 
-import utility.MyCharacter;
-
 /**
  * (Implement the Character class) The Character class is provided in the Java
  * library. Provide your own implementation for this class. Name the new class
@@ -89,9 +87,77 @@ public class PE_10_24_Implement_the_Character_class {
         System.out.println("MyCharacter.valueOf('1') = " + MyCharacter.valueOf('1').toString());
         System.out.println("MyCharacter.valueOf('q') = " + MyCharacter.valueOf('q').toString());
         System.out.println();
-        
-        
+    }
 
+    private static class MyCharacter {
+        private final char value;
 
+        MyCharacter(char value) {
+            this.value = value;
+        }
+
+        static int compare(char x, char y) {
+            if (x < y) return -1;
+            if (x > y) return 1;
+            return 0;
+        }
+
+        static MyCharacter valueOf(char c) {
+            return new MyCharacter(c);
+        }
+
+        char charValue() {
+            return value;
+        }
+
+        int compareTo(MyCharacter anotherCharacter) {
+            if (this.value < anotherCharacter.value) return -1;
+            if (this.value > anotherCharacter.value) return 1;
+            return 0;
+        }
+
+        boolean equals(MyCharacter anotherCharacter) {
+            return this.value == anotherCharacter.value;
+        }
+
+        int getNumericValue() {
+            if (isDigit()) return (this.value - '0');
+            else return -1;
+        }
+
+        boolean isLetter() {
+            return isUpperCase() || isLowerCase();
+        }
+
+        boolean isDigit() {
+            return value >= '0' && value <= '9';
+        }
+
+        boolean isLetterOrDigit() {
+            return isLetter() || isDigit();
+        }
+
+        boolean isLowerCase() {
+            return value >= 'a' && value <= 'z';
+        }
+
+        boolean isUpperCase() {
+            return value >= 'A' && value <= 'Z';
+        }
+
+        char toLowerCase() {
+            if (isUpperCase()) return (char) (value + ('a' - 'A'));
+            else return value;
+        }
+
+        char toUpperCase() {
+            if (isLowerCase()) return (char) (value - ('a' - 'A'));
+            else return value;
+        }
+
+        @Override
+        public String toString() {
+            return value + "";
+        }
     }
 }
