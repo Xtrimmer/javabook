e0b080dd965c8747f8910707a8f87a2757ce0050
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-27 08:56:52 -0600

    Consolidated Chapter 10 Exercise 23.

--- src/chapter_10/PE_10_23_Implement_the_String_class.java
+++ src/chapter_10/PE_10_23_Implement_the_String_class.java
@@ -1,6 +1,5 @@
 package chapter_10;
 
-import utility.MyString2;
 import java.util.Arrays;
 
 /**
@@ -65,6 +64,54 @@ public class PE_10_23_Implement_the_String_class {
     private static void printMyString2(MyString2 s) {
         for (int i = 0; i < s.toChars().length; i++) {
             System.out.print(s.toChars()[i]);
+        }
+    }
+
+    private static class MyString2 {
+        char[] chars;
+
+        MyString2(String s) {
+            chars = s.toCharArray();
+        }
+
+        static MyString2 valueOf(boolean b) {
+            return new MyString2(b ? "true" : "false");
+        }
+
+        int compare(String s) {
+            char[] chars = s.toCharArray();
+            int length = this.chars.length < chars.length
+                    ? this.chars.length : chars.length;
+            for (int i = 0; i < length; i++) {
+                if (this.chars[i] != chars[i]) {
+                    if (this.chars[i] < chars[i]) return -1;
+                    else return 1;
+                }
+            }
+            if (this.chars.length < chars.length) return -1;
+            else if (this.chars.length > chars.length) return 1;
+            else return 0;
+        }
+
+        MyString2 substring(int begin) {
+            char[] chars = new char[this.chars.length - begin];
+            System.arraycopy(this.chars, begin, chars, 0, chars.length);
+            return new MyString2(new String(chars));
+        }
+
+        MyString2 toUpperCase() {
+            char[] newChars = new char[chars.length];
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] >= 'a' && chars[i] <= 'z')
+                    newChars[i] = (char) (chars[i] - ('a' - 'A'));
+                else
+                    newChars[i] = chars[i];
+            }
+            return new MyString2(new String(newChars));
+        }
+
+        char[] toChars() {
+            return chars;
         }
     }
 }
