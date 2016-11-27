b69ded5eadf63c3a2d7558fc5d18e4ce44e752fb
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-27 08:28:41 -0600

    Consolidated Chapter 10 Exercise 11.

--- src/chapter_10/PE_10_11_Geometry_the_Circle2D_class.java
+++ src/chapter_10/PE_10_11_Geometry_the_Circle2D_class.java
@@ -1,7 +1,5 @@
 package chapter_10;
 
-import utility.Circle2D;
-
 /**
  * (Geometry: the Circle2D class) Define the Circle2D class that contains:
  *
@@ -43,5 +41,58 @@ public class PE_10_11_Geometry_the_Circle2D_class {
                 + c1.contains(new Circle2D(4, 5, 10.5))); //F
         System.out.println("   c1.overlaps(new Circle2D(3, 5, 2.3))):\n      "
                 + c1.overlaps(new Circle2D(3, 5, 2.3))); //F
+    }
+
+    private static class Circle2D {
+        private final double x;
+        private final double y;
+        private final double radius;
+
+        Circle2D() {
+            this(0, 0, 1);
+        }
+
+        Circle2D(double x, double y, double radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        double getX() {
+            return x;
+        }
+
+        double getY() {
+            return y;
+        }
+
+        double getRadius() {
+            return radius;
+        }
+
+        double getArea() {
+            return Math.PI * radius * radius;
+        }
+
+        double getPerimeter() {
+            return 2 * Math.PI * radius;
+        }
+
+        boolean contains(double x, double y) {
+            return distance(x, y) < radius;
+        }
+
+        boolean contains(Circle2D circle) {
+            return distance(circle.x, circle.y) <= Math.abs(radius - circle.radius);
+        }
+
+        boolean overlaps(Circle2D circle) {
+            return distance(circle.x, circle.y) <= radius + circle.radius
+                    && !contains(circle);
+        }
+
+        double distance(double x, double y) {
+            return Math.sqrt(Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2));
+        }
     }
 }
