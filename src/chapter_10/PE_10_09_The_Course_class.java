3f38c640f5f99aae72666995661fe15a52569539
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-27 08:18:29 -0600

    Consolidated Chapter 10 Exercise 09.

--- src/chapter_10/PE_10_09_The_Course_class.java
+++ src/chapter_10/PE_10_09_The_Course_class.java
@@ -1,7 +1,5 @@
 package chapter_10;
 
-import utility.Course;
-
 /**
  * (The Course class) Revise the Course class as follows:
  *
@@ -21,11 +19,64 @@ public class PE_10_09_The_Course_class {
         course.addStudent("Aaron");
         course.addStudent("Brett");
         course.addStudent("Charlie");
-
         course.dropStudent("Brett");
 
         for (int i = 0; i < course.getNumberOfStudents(); i++) {
             System.out.println("Student " + i + ": " + course.getStudents()[i]);
+        }
+    }
+
+    private static class Course {
+        private final String courseName;
+        private String[] students = new String[100];
+        private int numberOfStudents;
+
+        Course(String courseName) {
+            this.courseName = courseName;
+        }
+
+        void addStudent(String student) {
+            if (numberOfStudents >= students.length) {
+                String[] temp = new String[students.length * 2];
+                System.arraycopy(students, 0, temp, 0, students.length);
+                students = temp;
+            }
+            students[numberOfStudents] = student;
+            numberOfStudents++;
+        }
+
+        String[] getStudents() {
+            return students;
+        }
+
+        int getNumberOfStudents() {
+            return numberOfStudents;
+        }
+
+        String getCourseName() {
+            return courseName;
+        }
+
+        void dropStudent(String student) {
+            int index = getIndexOfStudent(student);
+            if (index >= 0) {
+                System.arraycopy(students, index + 1, students, index, numberOfStudents - index - 1);
+            }
+            students[--numberOfStudents] = null;
+        }
+
+        int getIndexOfStudent(String student) {
+            for (int i = 0; i < numberOfStudents; i++) {
+                if (students[i].equals(student)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        void clear() {
+            students = new String[100];
+            numberOfStudents = 0;
         }
     }
 }
