0a1888c863f812eb82e70b91d4dca2bc322c8df6
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-27 09:26:30 -0600

    Consolidated Chapter 10 Exercise 26.

--- src/chapter_10/PE_10_26_Calculator.java
+++ src/chapter_10/PE_10_26_Calculator.java
@@ -1,7 +1,5 @@
 package chapter_10;
 
-import static chapter_10.PE_10_25_New_string_split_method.*;
-
 /**
  * (Calculator) Revise Listing 7.9, Calculator.java, to accept an expression as
  * a string in which the operands and operator are separated by zero or more
@@ -15,7 +13,7 @@ public class PE_10_26_Calculator {
     public static void main(String[] args) {
         // Check number of strings passed
         if (args.length == 1)
-            args = split(args[0], "[+-*/]");
+            args = MyStringUtility.split(args[0], "[+-*/]");
         if (args.length != 3) {
             System.out.println(
                     "Usage: java Calculator operand1 operator operand2");
@@ -44,5 +42,63 @@ public class PE_10_26_Calculator {
         // Display result
         System.out.println(args[0] + ' ' + args[1] + ' ' + args[2]
                 + " = " + result);
+    }
+
+    private static class MyStringUtility {
+        static String[] split(String s, String regex) {
+            String[] delimiters = getDelimiters(regex);
+            String[] splitStrings = new String[s.length()];
+            StringBuilder stringBuilder = new StringBuilder(s);
+            int position = 0;
+            while (stringBuilder.length() > 0) {
+                if (!containsDelimiters(stringBuilder, delimiters)) {
+                    splitStrings[position++] = stringBuilder.toString();
+                    break;
+                }
+                int minIndex = stringBuilder.length();
+                int delimiterLength = 1;
+                for (String delimiter : delimiters) {
+                    int index = stringBuilder.indexOf(delimiter);
+                    if (index == 0) {
+                        minIndex = 0;
+                        break;
+                    } else if (index > 0 && index < minIndex) {
+                        minIndex = index;
+                        delimiterLength = delimiter.length();
+                    }
+                }
+                if (minIndex != 0) splitStrings[position++] = stringBuilder.substring(0, minIndex);
+                splitStrings[position++] = stringBuilder.substring(minIndex, minIndex + delimiterLength);
+                stringBuilder.delete(0, minIndex + delimiterLength);
+            }
+            String[] array = new String[position];
+            System.arraycopy(splitStrings, 0, array, 0, position);
+            return array;
+        }
+
+        private static boolean containsDelimiters(StringBuilder s, String[] delimiters) {
+            boolean hasDelimiters = false;
+            for (String delimiter : delimiters) {
+                hasDelimiters |= s.indexOf(delimiter) >= 0;
+            }
+            return hasDelimiters;
+        }
+
+        private static String[] getDelimiters(String regex) {
+            int bracketStart = regex.indexOf('[');
+            int bracketEnd = regex.indexOf(']');
+            int lastPosition = regex.length() - 1;
+            String[] delimiters;
+            if (bracketStart == 0 && bracketEnd == lastPosition) {
+                delimiters = new String[(bracketEnd - bracketStart - 1)];
+                for (int i = 0; i < regex.length() - 2; i++) {
+                    delimiters[i] = "" + regex.charAt(i + 1);
+                }
+            } else {
+                delimiters = new String[1];
+                delimiters[0] = regex;
+            }
+            return delimiters;
+        }
     }
 }
