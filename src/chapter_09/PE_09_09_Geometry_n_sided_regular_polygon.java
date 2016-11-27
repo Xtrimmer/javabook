4549b581c8be5f50bb77510730abe02fc11c9d76
Jeff Trimmer <jmt41860@ucmo.edu>
2016-11-26 19:11:17 -0600

    Modified Chapter 09 Exercise 09.

--- src/chapter_09/PE_09_09_Geometry_n_sided_regular_polygon.java
+++ src/chapter_09/PE_09_09_Geometry_n_sided_regular_polygon.java
@@ -1,7 +1,5 @@
 package chapter_09;
 
-import utility.RegularPolygon;
-
 /**
  * (Geometry: n-sided regular polygon) In an n-sided regular polygon, all sides
  * have the same length and all angles have the same degree (i.e., the polygon is
@@ -45,6 +43,68 @@ public class PE_09_09_Geometry_n_sided_regular_polygon {
         for (int i = 0; i < polygons.length; i++) {
             System.out.printf("Polygon-%d [Area: %7.3f, Perimeter: %2.0f]%n",
                     i, polygons[i].getArea(), polygons[i].getPerimeter());
+        }
+    }
+
+    private static class RegularPolygon {
+        private int n = 3;
+        private double side = 1;
+        private double x = 0;
+        private double y = 0;
+
+        RegularPolygon() {
+        }
+
+        RegularPolygon(int n, double side) {
+            this.n = n;
+            this.side = side;
+        }
+
+        RegularPolygon(int n, double side, double x, double y) {
+            this.n = n;
+            this.side = side;
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getN() {
+            return n;
+        }
+
+        public void setN(int n) {
+            this.n = n;
+        }
+
+        public double getSide() {
+            return side;
+        }
+
+        public void setSide(double side) {
+            this.side = side;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+
+        public double getPerimeter() {
+            return n * side;
+        }
+
+        public double getArea() {
+            return (n * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / n));
         }
     }
 }
