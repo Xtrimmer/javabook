f1009c0feb6b57e2f52ee260d89f9f949d8fc8f9
Jeff Trimmer <trimmerjeff@gmail.com>
2016-12-29 17:00:02 -0600

    Added Chapter 17 Exercise 01

---
+++ src/chapter_17/PE_17_01_Create_a_text_file.java
@@ -0,0 +1,12 @@
+package chapter_17;
+
+/**
+ * (Create a text file) Write a program to create a file named Exercise17_01.txt if
+ * it does not exist. Append new data to it if it already exists. Write 100 integers
+ * created randomly into the file using text I/O. Integers are separated by a space.
+ */
+public class PE_17_01_Create_a_text_file {
+    public static void main(String[] args) {
+
+    }
+}
