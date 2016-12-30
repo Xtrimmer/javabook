94bbd481fbda051e458cdc3322e7246bed397fa3
Jeff Trimmer <trimmerjeff@gmail.com>
2016-12-30 07:37:38 -0600

    Updated Chapter 17 Exercise 01.

--- src/chapter_17/PE_17_01_Create_a_text_file.java
+++ src/chapter_17/PE_17_01_Create_a_text_file.java
@@ -1,5 +1,7 @@
 package chapter_17;
 
+import java.io.*;
+
 /**
  * (Create a text file) Write a program to create a file named Exercise17_01.txt if
  * it does not exist. Append new data to it if it already exists. Write 100 integers
@@ -7,6 +9,12 @@ package chapter_17;
  */
 public class PE_17_01_Create_a_text_file {
     public static void main(String[] args) {
-
+        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream("Exercise17_01.txt", true))) {
+            for (int i = 0; i < 100; i++) {
+                printWriter.append((int) (Math.random() * 100) + " ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
