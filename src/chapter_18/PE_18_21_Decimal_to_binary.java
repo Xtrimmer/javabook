a0fd1345b4710e8d2748176016a72427eb32ff35
Jeff Trimmer <jmt41860@ucmo.edu>
2017-02-27 15:56:16 -0600

    Added Chapter 18 Exercise 21 & 22.

--- src/chapter_18/PE_18_21_Decimal_to_binary.java
+++ src/chapter_18/PE_18_21_Decimal_to_binary.java
@@ -3,59 +3,29 @@ package chapter_18;
 import java.util.Scanner;
 
 /**
- * (Decimal to hex) Write a recursive method that converts a decimal number into
- * a hex number as a string. The method header is:
+ * (Decimal to binary) Write a recursive method that converts a decimal number
+ * into a binary number as a string. The method header is:
  *
- *      public static String dec2Hex(int value)
+ *      public static String dec2Bin(int value)
  *
  * Write a test program that prompts the user to enter a decimal number and displays
- * its hex equivalent.
+ * its binary equivalent.
  */
 public class PE_18_21_Decimal_to_binary {
     public static void main(String[] args) {
         int number = promptUserForInteger();
-        System.out.println(dec2Hex(number));
+        System.out.println(dec2Bin(number));
     }
 
-    private static String dec2Hex(int value) {
-        return dec2Hex(value, "");
+    private static String dec2Bin(int value) {
+        return dec2Bin(value, "");
     }
 
-    private static String dec2Hex(int value, String hex) {
-        if (value == 0) return hex;
-        hex = digit2Hex(value % 16) + hex;
-        value /= 16;
-        return dec2Hex(value, hex);
-    }
-
-    private static String digit2Hex(int value) {
-        switch (value) {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-                return value + "";
-            case 10:
-                return "A";
-            case 11:
-                return "B";
-            case 12:
-                return "C";
-            case 13:
-                return "D";
-            case 14:
-                return "E";
-            case 15:
-                return "F";
-            default:
-                return "";
-        }
+    private static String dec2Bin(int value, String bin) {
+        if (value == 0) return bin;
+        bin = (value % 2 == 0 ? "0" : "1") + bin;
+        value /= 2;
+        return dec2Bin(value, bin);
     }
 
     private static int promptUserForInteger() {
@@ -69,4 +39,5 @@ public class PE_18_21_Decimal_to_binary {
         }
         return 0;
     }
+
 }
