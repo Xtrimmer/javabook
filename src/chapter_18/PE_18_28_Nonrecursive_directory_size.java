58cb7fef4188b4320a57d04bb0e394cfa4091ef5
Jeff Trimmer <jmt41860@ucmo.edu>
2017-03-25 16:45:41 -0500

    Finished Chapter 18 Exercise 28.

---
+++ src/chapter_18/PE_18_28_Nonrecursive_directory_size.java
@@ -0,0 +1,45 @@
+package chapter_18;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * (Nonrecursive directory size) Rewrite Listing 18.7, DirectorySize.java, without
+ * using recursion.
+ */
+public class PE_18_28_Nonrecursive_directory_size {
+    public static void main(String[] args) {
+        // Prompt the user to enter a directory or a file
+        System.out.print("Enter a directory or a file: ");
+        Scanner input = new Scanner(System.in);
+        String directory = input.nextLine();
+
+        // Display the size
+        System.out.println(getSize(new File(directory)) + " bytes");
+    }
+
+    public static long getSize(File file) {
+        long size = 0;
+        List<File> directories = new ArrayList<>();
+        if (file.isFile()) return file.length();
+        else if (file.isDirectory()) directories.add(file);
+        else {
+            System.out.println("Not a file nor directory!");
+            System.exit(0);
+        }
+        while (!directories.isEmpty()) {
+            File directory = directories.get(directories.size() - 1);
+            File[] subFiles = directory.listFiles();
+            if (subFiles != null) {
+                for (File subfile : subFiles) {
+                    if (subfile.isDirectory()) directories.add(subfile);
+                    else size += subfile.length();
+                }
+            }
+            directories.remove(directory);
+        }
+        return size;
+    }
+}
