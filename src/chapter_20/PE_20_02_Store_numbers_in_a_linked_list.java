e38f67ded5eafeda5eea378ad50c960d514ed7a9
Jeff Trimmer <trimmerjeff@gmail.com>
2017-04-21 09:38:02 -0500

    Added Chapter 20 Exercise 02.

---
+++ src/chapter_20/PE_20_02_Store_numbers_in_a_linked_list.java
@@ -0,0 +1,21 @@
+package chapter_20;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * (Store numbers in a linked list) Write a program that lets the user enter numbers
+ * from a graphical user interface and displays them in a text area, as shown in
+ * Figure 20.17a. Use a linked list to store the numbers. Do not store duplicate numbers.
+ * Add the buttons Sort, Shuffle, and Reverse to sort, shuffle, and reverse the list.
+ */
+public class PE_20_02_Store_numbers_in_a_linked_list extends Application{
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
