09c40aed84ab7d854c1ebf52e1f61ab8c2a5fd25
Jeff Trimmer <jmt41860@ucmo.edu>
2017-04-29 12:01:28 -0500

    Finished Chapter 20 Exercise 09.

---
+++ src/chapter_20/PE_20_09_Remove_the_largest_ball_first.java
@@ -0,0 +1,143 @@
+package chapter_20;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * (Remove the largest ball first) Modify Listing 20.6, MultipleBallApp.java to
+ * assign a random radius between 2 and 20 when a ball is created. When the -
+ * button is clicked, one of largest balls is removed.
+ */
+public class PE_20_09_Remove_the_largest_ball_first extends Application {
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support. Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        MultipleBallPane ballPane = new MultipleBallPane();
+        ballPane.setStyle("-fx-border-color: yellow");
+
+        Button btAdd = new Button("+");
+        Button btSubtract = new Button("-");
+        HBox hBox = new HBox(10);
+        hBox.getChildren().addAll(btAdd, btSubtract);
+        hBox.setAlignment(Pos.CENTER);
+
+        // Add or remove a ball
+        btAdd.setOnAction(e -> ballPane.add());
+        btSubtract.setOnAction(e -> ballPane.subtract());
+
+        // Pause and resume animation
+        ballPane.setOnMousePressed(e -> ballPane.pause());
+        ballPane.setOnMouseReleased(e -> ballPane.play());
+
+        // Use a scroll bar to control animation speed
+        ScrollBar sbSpeed = new ScrollBar();
+        sbSpeed.setMax(20);
+        sbSpeed.setValue(10);
+        ballPane.rateProperty().bind(sbSpeed.valueProperty());
+
+        BorderPane pane = new BorderPane();
+        pane.setCenter(ballPane);
+        pane.setTop(sbSpeed);
+        pane.setBottom(hBox);
+
+        // Create a scene and place the pane in the stage
+        Scene scene = new Scene(pane, 250, 150);
+        primaryStage.setTitle("MultipleBounceBall"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    private class MultipleBallPane extends Pane {
+        private final Timeline animation;
+
+        public MultipleBallPane() {
+            // Create an animation for moving the ball
+            animation = new Timeline(
+                    new KeyFrame(Duration.millis(50), e -> moveBall()));
+            animation.setCycleCount(Timeline.INDEFINITE);
+            animation.play(); // Start animation
+        }
+
+        public void add() {
+            Color color = new Color(Math.random(),
+                    Math.random(), Math.random(), 0.5);
+            getChildren().add(new Ball(30, 30, Math.random() * 18 + 2, color));
+        }
+
+        public void pause() {
+            animation.pause();
+        }
+
+        public void play() {
+            animation.play();
+        }
+
+        public DoubleProperty rateProperty() {
+            return animation.rateProperty();
+        }
+
+        public void subtract() {
+            if (getChildren().size() > 0) {
+                Circle maxBall = Collections.max((Collection<Ball>) (Collection<?>) (getChildren()));
+                getChildren().remove(maxBall);
+            }
+        }
+
+        protected void moveBall() {
+            for (Node node : this.getChildren()) {
+                Ball ball = (Ball) node;
+                // Check boundaries
+                if (ball.getCenterX() < ball.getRadius() ||
+                        ball.getCenterX() > getWidth() - ball.getRadius()) {
+                    ball.dx *= -1; // Change ball move direction
+                }
+                if (ball.getCenterY() < ball.getRadius() ||
+                        ball.getCenterY() > getHeight() - ball.getRadius()) {
+                    ball.dy *= -1; // Change ball move direction
+                }
+
+                // Adjust ball position
+                ball.setCenterX(ball.dx + ball.getCenterX());
+                ball.setCenterY(ball.dy + ball.getCenterY());
+            }
+        }
+    }
+
+    class Ball extends Circle implements Comparable<Circle> {
+        private double dx = 1, dy = 1;
+
+        Ball(double x, double y, double radius, Color color) {
+            super(x, y, radius);
+            setFill(color); // Set ball color
+        }
+
+        @Override
+        public int compareTo(Circle o) {
+            return Double.compare(this.getRadius(), o.getRadius());
+        }
+    }
+}
