375e09a6afacb1144b19db3dbbfa9cb50f189b37
Jeff Trimmer <jmt41860@ucmo.edu>
2017-05-21 05:46:46 -0500

    Updated Chapter 12 Exercise 05.

--- src/chapter_12/PE_12_05_IllegalTriangleException.java
+++ src/chapter_12/PE_12_05_IllegalTriangleException.java
@@ -1,7 +1,6 @@
 package chapter_12;
 
-import utility.IllegalTriangleException;
-import utility.Triangle_12_5;
+import textbook_listings.GeometricObject;
 
 /**
  * (IllegalTriangleException) Programming Exercise 11.1 defined the
@@ -15,11 +14,73 @@ import utility.Triangle_12_5;
  *      public Triangle(double side1, double side2, double side3)
  *              throws IllegalTriangleException {
  *              // Implement it
+ *
  *      }
  */
 public class PE_12_05_IllegalTriangleException {
     public static void main(String[] args) throws IllegalTriangleException {
+
+        // This triangle will NOT throw an exception.
+        Triangle triangle = new Triangle(3, 4, 5);
+
         // This triangle will throw an exception.
-        Triangle_12_5 triangle = new Triangle_12_5(1, 2, 4);
+        Triangle triangle2 = new Triangle(1, 2, 4);
+    }
+
+    static class Triangle extends GeometricObject {
+        private double side1;
+        private double side2;
+        private double side3;
+
+        public Triangle() throws IllegalTriangleException {
+            this(1.0, 1.0, 1.0);
+        }
+
+        public Triangle(double side1, double side2, double side3)
+                throws IllegalTriangleException {
+            if (side1 + side2 > side3
+                    && side2 + side3 > side1
+                    && side3 + side1 > side2) {
+                this.side1 = side1;
+                this.side2 = side2;
+                this.side3 = side3;
+            } else {
+                throw new IllegalTriangleException(side1 + ", " + side2 + ", " + side3);
+            }
+        }
+
+        public double getSide1() {
+            return side1;
+        }
+
+        public double getSide2() {
+            return side2;
+        }
+
+        public double getSide3() {
+            return side3;
+        }
+
+        @Override
+        public String toString() {
+            return "Triangle: side1 = " + side1 + " side2 = " + side2 +
+                    " side3 = " + side3;
+        }
+
+        public double getArea() {
+            double s = getPerimeter() / 2.0;
+            return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+        }
+
+        public double getPerimeter() {
+            return side1 + side2 + side3;
+        }
+    }
+
+    static class IllegalTriangleException extends Exception {
+
+        public IllegalTriangleException(String message) {
+            super("Invalid triangle: (" + message + ")");
+        }
     }
 }
